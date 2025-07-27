c87ce368c2e65d55c5f95f77a4af52b90acaec67
EunJi <univ217@naver.com>
2025-07-27 22:47:15 +0900

    :sparkle: JWT12: 세션 정보

--- src/main/java/com/example/springjwt/controller/MainController.java
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,8 +1,15 @@
 package com.example.springjwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.swing.*;
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -11,6 +18,15 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller" + username + role;
     }
 }
